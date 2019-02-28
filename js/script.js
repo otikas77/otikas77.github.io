672a431c524703e63e398fddb861a0bca151e17c
Anatoliy Bayda <otikas77@gmail.com>
2019-02-28 16:18:59 +0200

    Add main page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-let menuButton = document.getElementsByClassName('menuButton')[0]
-let dropDownMenu = document.getElementsByClassName('drop-menu')[0]
+let menuButton = document.getElementsByClassName('menuButton')[0];
+let dropDownMenu = document.getElementsByClassName('drop-menu')[0];
+let toTop = document.getElementsByClassName('toTop');
+let scrollMenu = document.getElementsByClassName("scroll");
+let myWork = document.getElementsByClassName('work-example');
+let progress = document.getElementsByClassName("progress");
+// SCROLLING --->
+scrollMenu[1].addEventListener('click', ()=>{
+    window.scrollTo({
+      top: document.getElementsByClassName('representation')[0].getBoundingClientRect().height,
+      behavior: "smooth"
+     })
+})
+
+scrollMenu[2].addEventListener('click', ()=>{
+    window.scrollTo({
+      top: document.getElementsByClassName('representation')[0].getBoundingClientRect().height + document.getElementsByClassName('about-me')[0].getBoundingClientRect().height,
+      behavior: "smooth"
+     })
+})
+
+scrollMenu[3].addEventListener('click', ()=>{
+    window.scrollTo({
+      top: document.getElementsByClassName('representation')[0].getBoundingClientRect().height + document.getElementsByClassName('about-me')[0].getBoundingClientRect().height + document.getElementsByClassName('section-education')[0].getBoundingClientRect().height + document.getElementsByClassName('section-skills')[0].getBoundingClientRect().height + 100,
+      behavior: "smooth"
+     })
+})
+//<--- SCROLLING
 
 menuButton.addEventListener("click", ()=>{
   if(dropDownMenu.style.display == "none"){
@@ -9,3 +35,33 @@ menuButton.addEventListener("click", ()=>{
     dropDownMenu.style.display = "none"
   }
 })
+
+for(let i=0; i<=toTop.length - 1; i++){
+  toTop[i].addEventListener("click", ()=>{
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+     })
+  })
+}
+
+
+for(let i=0; i<=myWork.length - 1; i++){
+  myWork[i].addEventListener("click", ()=>{
+    document.location.href = myWork[i].getAttribute('data-url');
+  })
+}
+
+// jquery
+
+$(document).ready(function(){
+
+	$(window).on("scroll", function(){
+
+    for(let i = 0; i<=progress.length -1; i++){
+          if($(window).scrollTop() >= 2489){
+            progress[i].style.width = progress[i].getAttribute("data-percent")
+          }
+        }
+      })
+    })
