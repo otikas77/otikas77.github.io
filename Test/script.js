92d43f940b596aca40b476f673be080889717a70
Anatoliy Bayda <otikas77@gmail.com>
2019-03-12 17:09:14 +0200

    add button

---
+++ Test/script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('button')[0].addEventListener("click",()=>{
+	document.getElementsByClassName('facebook')[0].style.width="100%"
+	document.getElementsByClassName('facebook')[0].style.height="100%"
+	document.getElementsByClassName('facebook')[0].style.transition='2s'
+	document.getElementsByClassName('facebook')[0].style.opacity='1'
+
+})
