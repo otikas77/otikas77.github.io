29f01a6fd675a3c0d1adff618c6d48371064fcac
Anatoliy Bayda <otikas77@gmail.com>
2019-03-12 17:34:50 +0200

    Del Test

--- Test/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementsByTagName('button')[0].addEventListener("click",()=>{
-	document.getElementsByClassName('facebook')[0].style.width="100%"
-	document.getElementsByClassName('facebook')[0].style.height="100%"
-	document.getElementsByClassName('facebook')[0].style.transition='2s'
-	document.getElementsByClassName('facebook')[0].style.opacity='1'
-
-})
